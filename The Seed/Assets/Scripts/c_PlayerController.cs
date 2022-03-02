de423970ac7f1f3d54ed34ecd9502f29bacfbf06
Jorge Silva <jorgemcs14@gmail.com>
2022-03-02 02:12:33 +0000

    Added New Characters, Character Controller Updated, Scene 1 Map Update

--- The Seed/Assets/Scripts/c_PlayerController.cs
+++ The Seed/Assets/Scripts/c_PlayerController.cs
@@ -8,6 +8,9 @@ public class c_PlayerController : MonoBehaviour {
     [SerializeField, Range(0.5f, 10.0f)] float walkingSpeed = 2.0f;
     [SerializeField, Range(0.5f, 10.0f)] float runningSpeed = 5.0f;
 
+    public Animator anim;
+    public GameObject body;
+
     private float currentSpeed = 0f;
     private m_InputManager input;
     private Rigidbody rb;
@@ -17,15 +20,19 @@ public class c_PlayerController : MonoBehaviour {
 
     public IInteractable interactable;
 
+    public bool useBlendTree = false;
+
     private void Start() {
         input = m_InputManager.Instance;
         rb = GetComponent<Rigidbody>();
         currentSpeed = walkingSpeed;
+
+        body.transform.localRotation = Quaternion.Euler(0f, cameraTransform.localEulerAngles.y, 0f);
     }
 
     private void Update() {
-        if(interactable != null) {
-            if(input.GetInteractionButton()) {
+        if (interactable != null) {
+            if (input.GetInteractionButton()) {
                 interactable.Interact();
             }
         }
@@ -37,34 +44,54 @@ public class c_PlayerController : MonoBehaviour {
 
     }
 
+    float lastRotation;
+    float startedRotating;
+
     private void HandleMovement() {
+
         Vector2 direction = input.GetMovementInput();
 
         float x = direction.x;
         float z = direction.y;
 
+        Vector3 animationMove = new Vector3(x, 0f, z);
+        animationMove = animationMove.normalized;
+
         Vector3 movePos = cameraTransform.right * x + cameraTransform.forward * z;
         movePos = movePos.normalized;
 
-        if(movePos != Vector3.zero) {
+        if (movePos != Vector3.zero) {
 
             currentSpeed = input.GetIsSprinting() ? runningSpeed : walkingSpeed;
 
+            anim.SetBool("isWalking", true);
+
             Vector3 velocity = new Vector3(movePos.x, rb.velocity.y, movePos.z);
             rb.velocity = Vector3.Scale(velocity, new Vector3(currentSpeed, 1f, currentSpeed));
+
+            lastRotation = (Mathf.Atan2(x, z) * 180) / Mathf.PI;
+
+            Quaternion initial = body.transform.rotation;
+            Quaternion target = Quaternion.Euler(0f, lastRotation + cameraTransform.localEulerAngles.y, 0f);
+
+            if (initial != target) {
+                startedRotating = Time.time;
+            }
+            body.transform.rotation = Quaternion.Lerp(initial, target, Time.time / (startedRotating + 10f));
         } else {
             rb.velocity = new Vector3(0f, rb.velocity.y, 0f);
+            anim.SetBool("isWalking", false);
         }
     }
 
     private void OnTriggerEnter(Collider other) {
-        if(other.GetComponent<IInteractable>() != null) {
+        if (other.GetComponent<IInteractable>() != null) {
             interactable = other.GetComponent<IInteractable>();
         }
     }
 
     private void OnTriggerExit(Collider other) {
-        if(other.GetComponent<IInteractable>() != null && interactable != null) {
+        if (other.GetComponent<IInteractable>() != null && interactable != null) {
             interactable = null;
         }
     }
